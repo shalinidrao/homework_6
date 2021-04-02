fbd6820f1df0cb33f251812a09ef227cfcd5e63a
Shalini Rao <sdrao@andrew.cmu.edu>
2021-04-01 20:43:53 -0400

    changing image and product details works

---
+++ main.js
@@ -0,0 +1,46 @@
+function Bun(flavor, glaze, quantity, image) {
+  this.flavor = flavor;
+  this.glaze = glaze;
+  this.quantity = quantity;
+  this.image_alt = "picture of" + this.type + "cinnamon bun";
+  this.image = image;
+}
+
+// number of items currently in cart
+var numItems = 0; 
+
+function setDetailImage() {
+  // sets default image on product detail page 
+  console.log("it's working");
+  document.getElementById("detailImage").setAttribute("src", "images/noglaze.jpeg");
+}
+
+
+// TODO: CHANGE FLAVOR IDS AND IMAGE NAMES
+function changeDetailImage() {
+  // changes the image on the detail page to match what glaze was selected
+  let glaze = document.getElementById("glaze");
+  if (glaze.value === "none") {
+    document.getElementById("detailImage").setAttribute("src", "images/noglaze.jpeg");
+  } else if (glaze.value === "sugarmilk") {
+    document.getElementById("detailImage").setAttribute("src", "images/sugarmilk.jpeg");
+  } else if (glaze.value === "vanillamilk") {
+    document.getElementById("detailImage").setAttribute("src", "images/vanillamilk.jpeg");
+  } else if (glaze.value === "doublechocolate") {
+    document.getElementById("detailImage").setAttribute("src", "images/doublechocolate.jpg");
+  } 
+}
+
+function changeBunTitle() {
+  let quantity = document.getElementById("quantity");
+  if (quantity.value === "one-pack") {
+    document.getElementById("detailTitle").innerHTML = " (Single)";
+  } else if (quantity.value === "three-pack") {
+    document.getElementById("detailTitle").innerHTML = " (3-Pack)";
+  } else if (quantity.value === "six-pack") {
+    document.getElementById("detailTitle").innerHTML = " (6-Pack)";
+  } else if (quantity.value === "twelve-pack") {
+    document.getElementById("detailTitle").innerHTML = " (12-Pack)";
+  } 
+}
+
